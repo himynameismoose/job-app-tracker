3c1d2b9fde0ff3de8efbeaaaf51867ccb8b4aed2
Mershelle Rivera <mershelle@Mershelles-MacBook-Pro.local>
2022-10-19 17:13:21 -0700

    Initial commit

---
+++ src/main/java/com/himynameismoose/jobapptracker/JobAppTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.himynameismoose.jobapptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAppTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAppTrackerApplication.class, args);
+	}
+
+}
